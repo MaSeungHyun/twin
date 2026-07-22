2aafaa608b4a74483e261f9445f2f6d143223af8
마승현 <seunghyun.ma@grapicar.com>
2026-07-22 15:48:12 +0900

    feat: 사용않는 컴포넌트 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { ControlShell } from "@/components/layout/ControlShell";
 import Viewport from "./components/viewport/Viewport";
-// import Fallback from "./components/viewport/Fallback";
 
 export default function App() {
   return (
     <main className="relative w-screen h-screen max-w-sceen max-h-screen">
-      {/* <ControlShell /> */}
+      <ControlShell />
       <Viewport />
-      {/* <Fallback /> */}
     </main>
   );
 }
