4385529f43e697edda41beed761ca72bf60217dc
마승현 <seunghyun.ma@grapicar.com>
2026-07-22 17:15:36 +0900

    feat: Fallback 전체화면 및 스프라이트

--- src/components/viewport/Fallback.tsx
+++ src/components/viewport/Fallback.tsx
@@ -5,7 +5,7 @@ export default function Fallback() {
   const { progress } = useProgress();
 
   return (
-    <Html center>
+    <Html center sprite fullscreen>
       <div className="flex flex-col flex-1  pointer-events-none select-none text-center text-sm tracking-wide w-screen h-screen items-center justify-center gap-4 bg-white backdrop-blur-sm">
         <img src={logo} alt="logo" className="w-1/5" />
         <div className="mt-8 mb-2 h-1.5 w-1/4 overflow-hidden rounded-full bg-white/15 border border-black/10">
