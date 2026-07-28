770cdafe4ba3ad757a1a23909c010cfd3c81b8fe
마승현 <seunghyun.ma@grapicar.com>
2026-07-28 11:19:36 +0900

    feat: postProcessing 무조건 true로 변경

--- src/components/viewport/ViewportScene.tsx
+++ src/components/viewport/ViewportScene.tsx
@@ -15,14 +15,10 @@ import SceneShadowSync from "./SceneShadowSync";
 const IS_DEV = import.meta.env.DEV;
 
 export default function ViewportScene() {
-  const postProcessingEnabled = useViewportTestStore(
-    (s) => s.postProcessingEnabled,
-  );
   const antialiasEnabled = useViewportTestStore((s) => s.antialiasEnabled);
   const gpuPowerPreference = useViewportTestStore((s) => s.gpuPowerPreference);
 
   /** Bloom/EffectComposer는 모바일·태블릿에서 VRAM·fill-rate 부담이 큼 */
-  const usePostProcessing = postProcessingEnabled && !isMobileDevice();
 
   return (
     <Canvas antialias={antialiasEnabled} powerPreference={gpuPowerPreference}>
@@ -34,16 +30,15 @@ export default function ViewportScene() {
       <OfficeCameraFlyer />
       <Office />
 
-      {usePostProcessing ? (
-        <EffectComposer multisampling={1}>
-          <Bloom
-            intensity={1.5}
-            threshold={1}
-            luminanceSmoothing={5}
-            mipmapBlur
-          />
-        </EffectComposer>
-      ) : null}
+      <EffectComposer multisampling={1}>
+        <Bloom
+          intensity={1.5}
+          threshold={1}
+          luminanceSmoothing={5}
+          mipmapBlur
+        />
+      </EffectComposer>
+
       {IS_DEV ? (
         <StatsGl className="pointer-events-none absolute top-18 right-4 z-2" />
       ) : null}
